e5bc8822ce2b455b5e4393ef1c56f952f37a7a98
Nikhil <nikhilpareek33@gmail.com>
2023-02-25 01:50:16 +0530

    modified train file with drive paths

--- train_ptsn.sh
+++ train_ptsn.sh
@@ -1,6 +1,6 @@
 python train_ptsn.py \
         --IMG_SIZE 224 \
-        --img_root_path '/content/image_captioning/data_path/coco_caption/IMAGE_COCO' \
-        --backbone_resume_path '/content/image_captioning/data_path/resume_model/swin_base_patch4_window7_224_22k.pth' \
-        --annotation_folder '/content/image_captioning/data_path/coco_caption/annotations/' \
+        --img_root_path '/content/gdrive/MyDrive/image_captioning_data' \
+        --backbone_resume_path '/content/gdrive/MyDrive/image_captioning_data/swin_base_patch4_window7_224_22k.pth' \
+        --annotation_folder '/content/gdrive/MyDrive/image_captioning_data/annotations' \
         --num_gpus 1
